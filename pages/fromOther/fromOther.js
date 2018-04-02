5fdb74d180dc44c5f51997fa844d90d44c9198b8
Unknown <yangxy81118@gmail.com>
2018-04-02 11:02:39 +0800

    加入分享页模板

---
+++ pages/fromOther/fromOther.js
@@ -0,0 +1,56 @@
+const commonTool = require("../../utils/common.js")
+
+Page({
+  data:{
+    change:""
+  },
+  onLoad: function () {
+    var that = this
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res)   
+        wx.request({
+          url: 'https://www.yubopet.top/login?code='+res.code,
+          success: function (loginRes) {
+            wx.setStorageSync('userId', loginRes.data)
+            initUserBaseData(loginRes.data)
+            wx.redirectTo({
+              url:'/pages/home/home'
+            })
+
+            // setTimeout(test, 1500, that)
+
+            // console.log("finish!")
+          }
+        })
+      }
+    })
+  }
+})
+
+function testTime(that){
+  that.setData({change:"changed"})
+}
+
+function initUserBaseData(userId){
+
+  //查询额度信息
+  var userId = wx.getStorageSync('userId')
+  wx.request({
+    url: 'https://www.yubopet.top/graphql/days',
+    method: 'POST',
+    data: '{user(userId:' + userId + '){limit daysCount} }',
+    header: {
+      'content-type': 'text/plain'
+    },
+    success: function (res) {
+      if (commonTool.checkError(res)) return
+      var u = res.data.data.user
+      wx.setStorageSync("daysLimit", u.limit)
+      wx.setStorageSync("daysCount", u.daysCount)
+      console.log("limit:"+u.limit+",count:"+u.daysCount)
+    }
+  });
+}
